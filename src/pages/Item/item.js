41986c9ddb6bc07f472b4cec593c56466d30552e
Thalisson Silva <54037942+thalissonms@users.noreply.github.com>
2019-12-17 11:19:14 -0300

    NEW RED

--- src/pages/Item/item.js
+++ src/pages/Item/item.js
@@ -12,7 +12,7 @@ export default function Item ({id, nome, quantidade, unidade, preço, openBox, m
                 }
             }
             if (unidade <= 0){
-               document.querySelector('.itemInfo').style.backgorundColor = '#F00';
+               document.getElementsByClassName('itemInfo').style.background = '#F00';
             }
         return(
             <div onClick={() => delEdit()} className="itemInfo">
